506471fe88f3865837664e6bc04bdc016f26d8fa
hachikuji-san <160812410+hachikuji-san@users.noreply.github.com>
2026-05-23 17:05:43 +0000

    Create UndefinedVariableException.php

---
+++ template/UndefinedVariableException.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Puchiko\template;
+
+/**
+ * Exception thrown when a template variable is not defined in the data context.
+ */
+class UndefinedVariableException extends \RuntimeException
+{
+}
